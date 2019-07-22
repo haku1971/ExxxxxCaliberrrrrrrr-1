4482fc6d8c63f72cfabfcc34848529bdc9533d08
haku1971 <53165001+haku1971@users.noreply.github.com>
2019-07-22 12:26:08 +0700

    Update màn hình chính

--- app/src/main/java/com/example/exxxxxcaliberrrrrrrr/MainActivity.java
+++ app/src/main/java/com/example/exxxxxcaliberrrrrrrr/MainActivity.java
@@ -6,33 +6,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
-    public static Intent myIntent;
-    boolean isRunninggame = true;
-    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-//        myIntent = new Intent(getApplicationContext(), Result.class);
+        setContentView(R.layout.result);
+//
+//        Intent myIntent = new Intent(this, Result.class);
 //        startActivity(myIntent);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        GameSurface gameSurface = new GameSurface(this);
-        this.setContentView(gameSurface);
-//        this.mContext=context;/
+
     }
 
     public static void callMe(String valuePassed) {
         Log.d("DEBUG","MainActivity was passed ["+ valuePassed +"]");
     }
 
-//    public void ChangeActivity(Context context){
-//        Intent myIntent = new Intent(context, Result.class);
-//        startActivity(myIntent);
-//    }
+
+    public void start(View view){
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        GameSurface gameSurface = new GameSurface(this);
+        this.setContentView(gameSurface);
+    }
+
 }
